5c86111ec29909615f8bf7facb5401ac889e7133
Jonathan Poblet <jonathanpoblet228@gmail.com>
2022-11-14 18:42:57 -0300

    crear-registro-y-login

--- frontend/src/mainstack/InitialStack.jsx
+++ frontend/src/mainstack/InitialStack.jsx
@@ -4,7 +4,8 @@ import Home from "../screens/Home/Home.jsx";
 import Games from "../screens/AllGames/AllGames.jsx";
 import Favorites from "../screens/Favourites/Favourites.jsx";
 import Account from "../screens/Account/Account.jsx";
-import AdivinaPalabra from "../Games/AdivinaPalabra/AdivinaPalabra.jsx";
+import SingUp from "../screens/SingUp/SingUp.jsx";
+import Login from "../screens/LogIn/LogIn.jsx";
 
 function InitalStack() {
   return (
@@ -14,7 +15,9 @@ function InitalStack() {
         <Route path="/games" element={<Games />} />
         <Route path="/favourites" element={<Favorites />} />
         <Route path="/account" element={<Account />} />
-        <Route path="/test" element={<AdivinaPalabra />} />
+        <Route path="/test" element={<SingUp />} />
+        <Route path="/singup" element={<SingUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
