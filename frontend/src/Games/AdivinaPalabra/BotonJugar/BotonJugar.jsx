31ac3798a16418902c760e2ee928318eb516bd46
Jonathan Poblet <jonathanpoblet228@gmail.com>
2022-11-22 20:28:37 -0300

    adivina-palabra-terminado

--- frontend/src/Games/AdivinaPalabra/BotonJugar/BotonJugar.jsx
+++ frontend/src/Games/AdivinaPalabra/BotonJugar/BotonJugar.jsx
@@ -1,7 +1,13 @@
 import PropTypes from "prop-types";
 
 const BotonJugar = ({ jugar, empezarJuego }) => {
-  return <button onClick={() => empezarJuego()}>{jugar}</button>;
+  return (
+    <button
+      onClick={() => empezarJuego()}
+      style={{ color: "#111", backgroundColor: "#F2CF8D", border: "2px solid #fff" }}>
+      {jugar}
+    </button>
+  );
 };
 
 BotonJugar.propTypes = {
