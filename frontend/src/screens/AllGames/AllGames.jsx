e83a52902c52b5a77eb15e8841cb9660c89a8489
Emanuel-Capo <emanuel_c400@hotmail.com>
2022-11-24 17:24:05 -0300

    responsiv3

--- frontend/src/screens/AllGames/AllGames.jsx
+++ frontend/src/screens/AllGames/AllGames.jsx
@@ -13,43 +13,47 @@ const AllGames = () => {
   return (
     <div className="games-content">
       <h2>{name}</h2>
-      <div className="screen-games">
-        <Outlet />
-      </div>
-      <div className="text-start">
-        <h3>Descripción:</h3>
-        <p>{description}</p>
-        <div className="d-flex">
-          <span className="circle">+{minAge}</span>
-          <span className="circle">
-            <i className="bi bi-mouse2"></i>
-          </span>
-          <span className="circle">
-            <i className="bi bi-hand-index-thumb"></i>
-          </span>
+      <div className="desktop">
+        <div className="screen-games">
+          <Outlet />
         </div>
-      </div>
-      <div className="ranking">
-        <h4>Nombre</h4>
-        <h4>Puntuación</h4>
-        <p>Pedro</p>
-        <p>1234</p>
-        <p>Juan</p>
-        <p>1234</p>
-        <p>Ana</p>
-        <p>1234</p>
-        <p>Marcelo</p>
-        <p>1234</p>
-        <p>Carla</p>
-        <p>1234</p>
-      </div>
-      <div className="favorites">
-        <img src={fav} alt="" />
-        <p>Agregar a tu lista de favoritos</p>
-      </div>
-      <div className="qualify">
-        <h4>Califica el juego</h4>
-        <Rate rating={rating} onRating={rate => setRating(rate)} />
+        <div>
+          <div className="text-start">
+            <h3>Descripción:</h3>
+            <p>{description}</p>
+            <div className="d-flex">
+              <span className="circle">+{minAge}</span>
+              <span className="circle">
+                <i className="bi bi-mouse2"></i>
+              </span>
+              <span className="circle">
+                <i className="bi bi-hand-index-thumb"></i>
+              </span>
+            </div>
+          </div>
+          <div className="ranking">
+            <h4>Nombre</h4>
+            <h4>Puntuación</h4>
+            <p>Pedro</p>
+            <p>1234</p>
+            <p>Juan</p>
+            <p>1234</p>
+            <p>Ana</p>
+            <p>1234</p>
+            <p>Marcelo</p>
+            <p>1234</p>
+            <p>Carla</p>
+            <p>1234</p>
+          </div>
+          <div className="favorites">
+            <img src={fav} alt="" />
+            <p>Agregar a tu lista de favoritos</p>
+          </div>
+          <div className="qualify">
+            <h4>Califica el juego</h4>
+            <Rate rating={rating} onRating={rate => setRating(rate)} />
+          </div>
+        </div>
       </div>
     </div>
   );
