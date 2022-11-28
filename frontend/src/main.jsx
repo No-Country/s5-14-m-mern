3a70442f2ccae34dff289b6fdfb7f80fb9fc3146
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-11-28 18:07:23 +0100

    styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "../src/redux/store.js";
-import "./styles.sass";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
