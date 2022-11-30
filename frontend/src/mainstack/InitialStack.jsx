1f5361da5abb2d368d812b3b54f031e38b5b58a1
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-01 00:48:14 +0100

    routes added

--- frontend/src/mainstack/InitialStack.jsx
+++ frontend/src/mainstack/InitialStack.jsx
@@ -1,23 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import RequireAuth from "../Routing/RequireAuth.jsx";
 import Home from "../screens/Home/Home.jsx";
 import Games from "../screens/AllGames/AllGames.jsx";
 import Account from "../screens/Account/Account.jsx";
 import SignUp from "../screens/SignUp/SignUp.jsx";
 import Login from "../screens/LogIn/LogIn.jsx";
-import TriviaPage from "../Games/Trivia/index.jsx";
-import { PPTApp } from "../Games/piedraPapelTijeras/PPTApp.jsx";
+import AdminPannel from "../screens/AdminPanel/AdminPannel.jsx";
 import Notifications from "../screens/Notifications/Notifications.jsx";
 import Messages from "../screens/Messages/Messages.jsx";
 import Layout from "../screens/Layout/Layout.jsx";
+import Favourites from "../screens/Favourites/Favourites.jsx";
+
+import TriviaPage from "../Games/Trivia/index.jsx";
+import { PPTApp } from "../Games/piedraPapelTijeras/PPTApp.jsx";
 import AdivinaPalabra from "../Games/AdivinaPalabra/AdivinaPalabra.jsx";
 import Rompecabezas from "../Games/Rompecabezas/Rompecabezas.jsx";
 import LightGame from "../Games/juegoLuces/LightGame.jsx";
-import Favourites from "../screens/Favourites/Favourites.jsx";
+
 import MessageUser from "../components/MessageComponents/MessangerUser/index.jsx";
-import RequireAuth from "../Routing/RequireAuth.jsx";
 import GameForm from "../components/PagesComponents/AdminPannel/GameForm/GameForm.jsx";
-import GameList from "../components/PagesComponents/AdminPannel/GameList.jsx";
+import GameList from "../components/PagesComponents/AdminPannel/GameList/GameList.jsx";
 
 function InitalStack() {
   return (
@@ -26,23 +29,23 @@ function InitalStack() {
         <Routes element={<Layout />}>
           <Route exact path="/" element={<Home />} />
 
-          <Route element={<RequireAuth allowedRole="user"/>}>
+          <Routes element={<RequireAuth allowedRole="user" />}>
             <Route exact path="/account" element={<Account />} />
             <Route path="/favourites" element={<Favourites />} />
             <Route path="/notifications" element={<Notifications />} />
             <Route path="/messages" element={<Messages />} />
             <Route path="/messages/:userId" element={<MessageUser />} />
             <Route path="/account" element={<Account />} />
-          </Route>
+          </Routes>
 
-          <Route element={<RequireAuth allowedRole="user"/>}>
+          <Routes element={<RequireAuth allowedRole="user" />}>
             <Route path="/admin" element={<AdminPannel />}>
               <Route index element={<GameList />} />
               <Route path="game-manage" element={<GameForm />} />
               <Route path="game-manage/:id" element={<GameForm />} />
             </Route>
-          <Route/> 
-          
+          </Routes>
+
           <Route path="/games" element={<Games />}>
             <Route path="ppt" element={<PPTApp />} />
             <Route path="adivinapalabra" element={<AdivinaPalabra />} />
@@ -51,13 +54,14 @@ function InitalStack() {
             <Route path="juegoLuces" element={<LightGame />} />
             {/* cargar juegos */}
           </Route>
-        </Route>
+        </Routes>
+
         <Route path="/test" element={<AdivinaPalabra />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-    );
+  );
 }
 
 export default InitalStack;
