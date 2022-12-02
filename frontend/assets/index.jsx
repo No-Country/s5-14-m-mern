fbcc5eddb45756ab080a49e91ae7c583a2b46b0d
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-02 19:40:19 +0100

    gamepad export

--- frontend/assets/index.jsx
+++ frontend/assets/index.jsx
@@ -7,3 +7,4 @@ export { default as trash } from "./Icons/trash.svg";
 export { default as touch } from "./Icons/touch.svg";
 export { default as keyboard } from "./Icons/keyboard.svg";
 export { default as defaultImage } from "./Icons/defaultImage.svg";
+export { default as gamepad } from "./Icons/gamepad.svg";
