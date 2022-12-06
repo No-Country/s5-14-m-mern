be9588b4d778dc58c6a520aa54c17b09f536f099
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-06 14:27:15 +0100

    communicating

--- frontend/src/services/socket.jsx
+++ frontend/src/services/socket.jsx
@@ -7,4 +7,8 @@ const socket = io("http://localhost:8000", {
   query: { token }
 });
 
+socket.on("connect", () => {
+  console.log(`you are connected with id: ${socket.id}`);
+});
+
 export default socket;
