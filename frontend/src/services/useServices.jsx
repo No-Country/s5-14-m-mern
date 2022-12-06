f6e24fe008c4f251de1740fbd91c144d8576d20b
Emanuel-Capo <emanuel_c400@hotmail.com>
2022-12-06 02:20:29 -0300

    perfil

--- frontend/src/services/useServices.jsx
+++ frontend/src/services/useServices.jsx
@@ -3,7 +3,8 @@ import { getToken } from "./localStorage.jsx";
 
 const useServices = () => {
   // const BASE_URL = import.meta.env.VITE_API_URL;
-  const BASE_URL = "http://localhost:8000";
+  // const BASE_URL = "http://localhost:8000";
+  const BASE_URL = "https://ludensapi.vercel.app";
 
   const routeUrl = {
     auth: BASE_URL + "/api/auth",
