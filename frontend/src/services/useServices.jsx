ad652c0a12ce43cc63cf22694ad28c8829049625
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-07 23:13:57 +0100

    route fixing useservices

--- frontend/src/services/useServices.jsx
+++ frontend/src/services/useServices.jsx
@@ -115,7 +115,7 @@ const useServices = () => {
     getChathistory: userId => apiProtected().get(`${routeUrl.chat}/${userId}`),
     setChathistory: (chatId, data) => apiProtected().post(`${routeUrl.chat}/${chatId}`, data),
     socketSend: (currentChannel, data) =>
-      apiProtected().post(`http://localhost:8000/api/message?channel=${currentChannel}`, data)
+      apiProtected().post(`${BASE_URL}/api/message?channel=${currentChannel}`, data)
   };
   return {
     auth,
