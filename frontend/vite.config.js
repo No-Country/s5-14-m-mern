ba82c71d60aec01ec6add4aed3e78d14e483e7f4
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-08 19:37:07 +0100

    config vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: assetInfo => {
-          let extType = assetInfo.name.split(".").at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        }
-      }
-    }
-  }
+  plugins: [react()]
 });
