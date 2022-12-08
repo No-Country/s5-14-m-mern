70e65194810cf4afafbb67cd9df7873dbe2ebf62
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-08 12:46:34 +0100

    avatar

--- frontend/src/components/NotificationsComponents/FriendsNotification.jsx
+++ frontend/src/components/NotificationsComponents/FriendsNotification.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import style from "./friendsNotification.module.sass";
-import avatarI from "../../../assets/avatar0.svg";
+import avatarI from "../../../assets/avatar0";
 
 const FriendsNotification = ({ data, accept, refuse }) => {
   return (
