a526dabfc53ca22d025ac7897e8021c09016f76a
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-08 20:34:04 +0100

    avatar re render changed

--- frontend/src/components/PagesComponents/Avatar/Avatar.jsx
+++ frontend/src/components/PagesComponents/Avatar/Avatar.jsx
@@ -1,46 +1,41 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 
 const Avatar = ({ className, avatar, onClick }) => {
-  const [ava, setAva] = useState(
-    "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522154/avatar0_jroqso.svg"
-  );
-
+  let source;
   switch (avatar) {
     case "avatar0":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522154/avatar0_jroqso.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522154/avatar0_jroqso.svg";
       break;
     case "avatar1":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522154/avatar1_wpdiqs.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522154/avatar1_wpdiqs.svg";
       break;
     case "avatar2":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar2_qvlhmi.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar2_qvlhmi.svg";
       break;
     case "avatar3":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar3_xnzjfw.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar3_xnzjfw.svg";
       break;
     case "avatar4":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar4_fbqzxi.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar4_fbqzxi.svg";
       break;
     case "avatar5":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar5_wdswra.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar5_wdswra.svg";
       break;
     case "avatar6":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar6_ilw3eg.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar6_ilw3eg.svg";
       break;
     case "avatar7":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar7_sutnbh.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522152/avatar7_sutnbh.svg";
       break;
     case "avatar8":
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522186/avatar8_tzjhkz.svg");
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522186/avatar8_tzjhkz.svg";
       break;
 
     default:
-      setAva("https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522154/avatar0_jroqso.svg");
-      break;
+      source = "https://res.cloudinary.com/ddg3a37dp/image/upload/v1670522154/avatar0_jroqso.svg";
   }
 
-  return <img className={className} src={ava} onClick={onClick || null} />;
+  return <img className={className} src={source} onClick={onClick || null} />;
 };
 
 export default Avatar;
