0ee82d63be9edcad5cdf70b3960f63476093a43b
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-12-08 18:06:34 +0100

    image

--- frontend/src/screens/Account/Account.jsx
+++ frontend/src/screens/Account/Account.jsx
@@ -159,7 +159,11 @@ const Account = () => {
           <>
             <form onSubmit={handleSubmit}>
               <div className={style.account}>
-                <img className={style.mainAvatar} src={avatar || avatarDef} alt="" />
+                <img
+                  className={style.mainAvatar}
+                  src={getUserImage(userInfo.avatar) || getUserImage("avatar0")}
+                  alt=""
+                />
                 <div className={style.user}>
                   <input
                     className={style.input}
